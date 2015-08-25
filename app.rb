c88a6884fbfdfe7e5ad3973734b742412da3cb44
travis crampton & leah klamkin <student@epicodus.com>
2015-08-25 10:22:05 -0700

    add index.erb and update app.rb

--- app.rb
+++ app.rb
@@ -5,3 +5,13 @@ require('sinatra/activerecord')
 require("./lib/question")
 require("./lib/survey")
 require("pg")
+
+
+get('/') do
+  erb(:index)
+end
+
+get('/survey') do
+  @surveys = Survey.all()
+  erb(:survey_dash)
+end
